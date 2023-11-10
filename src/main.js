1ca268ba79a34723585c5b2a0a8e60d9c7de0e22
Isabellaviana28 <139775674+Isabellaviana28@users.noreply.github.com>
2023-11-09 22:53:14 -0300

    inicio do projeto3

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
-import App from './components/App.js';
+import data from "./data/pokemon/pokemon.js";
 
-document.getElementById('root').appendChild(App());
+//document.getElementById('items').appendChild(App());//
+
+        // Use JavaScript para definir a fonte da imagem com a URL da API
+       console.log (data)
+        const container = document.getElementById ('pokemon');
+        const pokemonsDuplicados = [...data.items, ...data.items];
+          for (let pokemon of pokemonsDuplicados)  {
+            const img = document.createElement('img');
+            img.src = pokemon.image;
+            container.appendChild(img);
+    
+          }
+
+        
