217efdda685c71e872a03407bce14d7bf0ae6ac5
Isabellaviana28 <139775674+Isabellaviana28@users.noreply.github.com>
2023-11-17 09:54:17 -0300

    adcionando divs

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
 import data from "./data/pokemon/pokemon.js";
 
 //document.getElementById('items').appendChild(App());//
-
         // Use JavaScript para definir a fonte da imagem com a URL da API
        console.log (data)
         const container = document.getElementById ('pokemon');
         const pokemonsDuplicados = [...data.items, ...data.items];
           for (let pokemon of pokemonsDuplicados)  {
+            const div = document.createElement('div'); 
             const img = document.createElement('img');
             img.src = pokemon.image;
-            container.appendChild(img);
-    
+            div.appendChild(img);
+            container.appendChild(div);
+            
           }
+               
 
         
